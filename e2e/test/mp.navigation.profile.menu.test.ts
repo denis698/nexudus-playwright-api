e09328150ee9420e725c78bb66c77615c4a3f8af
denis <denis@nexudus.com>
2026-01-27 14:20:29 +0000

    adding ne @NPA_002 tests

--- e2e/test/mp.navigation.profile.menu.test.ts
+++ e2e/test/mp.navigation.profile.menu.test.ts
@@ -5,12 +5,24 @@ test.beforeEach(async ({ mpLoginPage,mpDashboardPage }) => {
   await mpLoginPage.navigateTo(process.env.MP_TEST_LOGIN_PAGE_URL);
   await mpLoginPage.verifyAt();
   await mpLoginPage.login(String(process.env.MP_LOCATION_PASSWORD));
-  await mpLoginPage.loginAs(String(process.env.MP_TEST_USERNAME), String(process.env.MP_TEST_PASSWORD));
-  await mpDashboardPage.verifyAt();
 });
 
 test.describe('Navigation->User Profile Menu', () => {
-  test(`@15002 @smoke @mp.navigation - should be able to access profile penu`, async ({mpDashboardPage}) => {
+  test(`@NPA_002 @smoke @mp.navigation - admin should be able to access Profile Menu->Page editor->Admin`, async ({mpLoginPage,mpDashboardPage}) => {
+    await mpLoginPage.loginAs(String(process.env.MP_TEST_ADMIN_USERNAME), String(process.env.MP_TEST_ADMIN_PASSWORD));
+    await mpDashboardPage.verifyAt();
+    await mpDashboardPage.verifyProfileMenu(menuData.dashboard_admin_menu);
+  });
+
+  test(`@NPA_002 @smoke @mp.navigation - admin should be able to access Profile Menu->Switch account`, async ({mpLoginPage,mpDashboardPage}) => {
+    await mpLoginPage.loginAs(String(process.env.MP_TEST_ADMIN_USERNAME), String(process.env.MP_TEST_ADMIN_PASSWORD));
+    await mpDashboardPage.verifyAt();
+    await mpDashboardPage.verifyProfileMenu(menuData.dashboard_admin_menu);
+  });
+
+  test(`@NPA_002 @smoke @mp.navigation - user should be able to access Profile Menu`, async ({mpLoginPage,mpDashboardPage}) => {
+    await mpLoginPage.loginAs(String(process.env.MP_TEST_USERNAME), String(process.env.MP_TEST_PASSWORD));
+    await mpDashboardPage.verifyAt();
     await mpDashboardPage.verifyProfileMenu(menuData.dashboard_menu);
   });
 
